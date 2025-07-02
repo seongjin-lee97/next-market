bbea2bb5ecf28b3558b0f1baeb4bbe421136ce81
seongjinlee97 <seongjinlee97@gmail.com>
2025-07-02 11:22:16 +0900

    minor fix

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import useAuth from "../../utils/useAuth";
-import ImgInput from "../.../components/ImgInput";
+import ImgInput from "../../components/ImgInput";
 
 const CreateItem = () => {
   const [title, setTitle] = useState("");
