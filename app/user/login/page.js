d3da5dfe1d08cf6428e0881d2bf413e5d14ad87a
seongjinlee97 <seongjinlee97@gmail.com>
2025-07-03 10:32:41 +0900

    plz

--- app/user/login/page.js
+++ app/user/login/page.js
@@ -1,69 +1,12 @@
-"use client";
-import { useState } from "react";
-import Head from "next/head";
+import MyPage from "./myPage";
 
 export const metadata = {
   title: "로그인 페이지",
   description: "Next Market 로그인 페이지입니다.",
 };
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_URL}/api/user/login`,
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-        }
-      );
-      const jsonData = await response.json();
-      localStorage.setItem("token", jsonData.token);
-      alert(jsonData.message);
-    } catch {
-      alert("로그인 실패");
-    }
-  };
-
-  return (
-    <div>
-      {/* <Head>
-        <title>로그인 페이지</title>
-        <meta name="description" content="로그인 페이지입니다" />
-      </Head> */}
-      <h1 className="page-title">로그인</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="text"
-          name="email"
-          placeholder="메일 주소"
-          required
-        />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="text"
-          name="password"
-          placeholder="비밀번호"
-          required
-        />
-        <button>로그인</button>
-      </form>
-    </div>
-  );
+const Login = (context) => {
+  return <MyPage {...context} />;
 };
 
 export default Login;
