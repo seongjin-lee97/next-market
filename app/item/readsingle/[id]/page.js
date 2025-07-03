79da18ce5025928707b1278a89a1c78da86af027
seongjinlee97 <seongjinlee97@gmail.com>
2025-07-03 10:09:22 +0900

    import Head

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 
 const getSingleItem = async (id) => {
   const response = await fetch(
--- app/user/login/page.js
+++ app/user/login/page.js
@@ -1,5 +1,7 @@
 "use client";
 import { useState } from "react";
+import Head from "next/head";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
